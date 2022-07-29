d0aa3dc708e3feb973e1d511abb1d9ae135fc772
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-07-29 14:16:36 -0500

    Add files via upload

---
+++ songs.py
@@ -0,0 +1,1654 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+
+body {
+   text-align: center;
+}
+
+table{
+  background-color: white;
+  border-radius: 3px;
+  font-family: 'Open Sans', sans-serif;
+  font-size:18px;
+  background-color:#34495e;
+  table-layout: fixed;
+  margin:30px auto;
+
+}
+
+td, th{
+  padding:18px;
+  border-radius: 3px;
+  background-color: #2c3e50;
+  color:white;
+  box-shadow: 2px 2px 5px #34495e;
+  text-align:center;
+}
+
+th{
+  background-color: #2980b9;
+}
+</style>
+
+<table><tbody>
+<tr>
+<th> TrackId </th>
+<th> Name </th>
+<th> AlbumId </th>
+<th> MediaTypeId </th>
+<th> GenreId </th>
+<th> Composer </th>
+<th> Milliseconds </th>
+<th> Bytes </th>
+<th> UnitPrice </th>
+</tr>
+<tr>
+<td> 1 </td>
+<td> For Those About To Rock (We Salute You) </td>
+<td> 1 </td>
+<td> 1 </td>
+<td> 1 </td>
+<td> Angus Young, Malcolm Young, Brian Johnson </td>
+<td> 343719 </td>
+<td> 11170334 </td>
+<td> 0.99 </td>
+</tr>
+<tr>
+<td> 3 </td>
+<td> Fast As a Shark </td>
+<td> 3 </td>
+<td> 2 </td>
+<td> 1 </td>
+<td> F. Baltes, S. Kaufman, U. Dirkscneider & W. Hoffman </td>
+<td> 230619 </td>
+<td> 3990994 </td>
+<td> 0.99 </td>
+</tr>
+<tr>
+<td> 4 </td>
+<td> Restless and Wild </td>
+<td> 3 </td>
+<td> 2 </td>
+<td> 1 </td>
+<td> F. Baltes, R.A. Smith-Diesel, S. Kaufman, U. Dirkscneider & W. Hoffman </td>
+<td> 252051 </td>
+<td> 4331779 </td>
+<td> 0.99 </td>
+</tr>
+<tr>
+<td> 5 </td>
+<td> Princess of the Dawn </td>
+<td> 3 </td>
+<td> 2 </td>
+<td> 1 </td>
+<td> Deaffy & R.A. Smith-Diesel </td>
+<td> 375418 </td>
+<td> 6290521 </td>
+<td> 0.99 </td>
+</tr>
+<tr>
+<td> 6 </td>
+<td> Put The Finger On You </td>
+<td> 1 </td>
+<td> 1 </td>
+<td> 1 </td>
+<td> Angus Young, Malcolm Young, Brian Johnson </td>
+<td> 205662 </td>
+<td> 6713451 </td>
+<td> 0.99 </td>
+</tr>
+</tbody></table>
+<table><tbody>
+<tr>
+<th> TrackId </th>
+<th> Name </th>
+<th> AlbumId </th>
+<th> MediaTypeId </th>
+<th> GenreId </th>
+<th> Composer </th>
+<th> Milliseconds </th>
+<th> Bytes </th>
+<th> UnitPrice </th>
+</tr>
+<tr>
+<td> 7 </td>
+<td> Let's Get It Up </td>
+<td> 1 </td>
+<td> 1 </td>
+<td> 1 </td>
+<td> Angus Young, Malcolm Young, Brian Johnson </td>
+<td> 233926 </td>
+<td> 7636561 </td>
+<td> 0.99 </td>
+</tr>
+<tr>
+<td> 8 </td>
+<td> Inject The Venom </td>
+<td> 1 </td>
+<td> 1 </td>
+<td> 1 </td>
+<td> Angus Young, Malcolm Young, Brian Johnson </td>
+<td> 210834 </td>
+<td> 6852860 </td>
+<td> 0.99 </td>
+</tr>
+<tr>
+<td> 9 </td>
+<td> Snowballed </td>
+<td> 1 </td>
+<td> 1 </td>
+<td> 1 </td>
+<td> Angus Young, Malcolm Young, Brian Johnson </td>
+<td> 203102 </td>
+<td> 6599424 </td>
+<td> 0.99 </td>
+</tr>
+</tbody></table>
+<table><tbody>
+<tr>
+<th> AlbumId </th>
+<th> Total Tracks </th>
+</tr>
+<tr>
+<td> 1 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 3 </td>
+<td> 3 </td>
+</tr>
+<tr>
+<td> 4 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 5 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 6 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 7 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 8 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 9 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 10 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 11 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 12 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 13 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 14 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 15 </td>
+<td> 5 </td>
+</tr>
+<tr>
+<td> 16 </td>
+<td> 7 </td>
+</tr>
+<tr>
+<td> 17 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 18 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 19 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 20 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 21 </td>
+<td> 18 </td>
+</tr>
+<tr>
+<td> 22 </td>
+<td> 3 </td>
+</tr>
+<tr>
+<td> 23 </td>
+<td> 34 </td>
+</tr>
+<tr>
+<td> 24 </td>
+<td> 23 </td>
+</tr>
+<tr>
+<td> 25 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 26 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 27 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 28 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 29 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 30 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 31 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 32 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 33 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 34 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 35 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 36 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 37 </td>
+<td> 20 </td>
+</tr>
+<tr>
+<td> 38 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 39 </td>
+<td> 21 </td>
+</tr>
+<tr>
+<td> 40 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 41 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 42 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 43 </td>
+<td> 7 </td>
+</tr>
+<tr>
+<td> 44 </td>
+<td> 6 </td>
+</tr>
+<tr>
+<td> 45 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 46 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 47 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 48 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 49 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 50 </td>
+<td> 4 </td>
+</tr>
+<tr>
+<td> 51 </td>
+<td> 22 </td>
+</tr>
+<tr>
+<td> 52 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 53 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 54 </td>
+<td> 20 </td>
+</tr>
+<tr>
+<td> 55 </td>
+<td> 20 </td>
+</tr>
+<tr>
+<td> 56 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 57 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 58 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 59 </td>
+<td> 7 </td>
+</tr>
+<tr>
+<td> 60 </td>
+<td> 7 </td>
+</tr>
+<tr>
+<td> 61 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 62 </td>
+<td> 7 </td>
+</tr>
+<tr>
+<td> 63 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 64 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 65 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 66 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 67 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 68 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 69 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 70 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 71 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 72 </td>
+<td> 18 </td>
+</tr>
+<tr>
+<td> 73 </td>
+<td> 30 </td>
+</tr>
+<tr>
+<td> 74 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 75 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 76 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 77 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 78 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 79 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 80 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 81 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 82 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 83 </td>
+<td> 24 </td>
+</tr>
+<tr>
+<td> 84 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 85 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 86 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 87 </td>
+<td> 3 </td>
+</tr>
+<tr>
+<td> 88 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 89 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 90 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 91 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 92 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 93 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 94 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 95 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 96 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 97 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 98 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 99 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 100 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 101 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 102 </td>
+<td> 18 </td>
+</tr>
+<tr>
+<td> 103 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 104 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 105 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 106 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 107 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 108 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 109 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 110 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 111 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 112 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 113 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 114 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 115 </td>
+<td> 19 </td>
+</tr>
+<tr>
+<td> 116 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 117 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 118 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 119 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 120 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 121 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 122 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 123 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 124 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 125 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 126 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 127 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 128 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 129 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 130 </td>
+<td> 7 </td>
+</tr>
+<tr>
+<td> 131 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 132 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 133 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 134 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 135 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 136 </td>
+<td> 7 </td>
+</tr>
+<tr>
+<td> 137 </td>
+<td> 5 </td>
+</tr>
+<tr>
+<td> 138 </td>
+<td> 4 </td>
+</tr>
+<tr>
+<td> 139 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 140 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 141 </td>
+<td> 57 </td>
+</tr>
+<tr>
+<td> 142 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 143 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 144 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 145 </td>
+<td> 18 </td>
+</tr>
+<tr>
+<td> 146 </td>
+<td> 18 </td>
+</tr>
+<tr>
+<td> 147 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 148 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 149 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 150 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 151 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 152 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 153 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 154 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 155 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 156 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 157 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 158 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 159 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 160 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 161 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 162 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 163 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 164 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 165 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 166 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 167 </td>
+<td> 21 </td>
+</tr>
+<tr>
+<td> 168 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 169 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 170 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 171 </td>
+<td> 2 </td>
+</tr>
+<tr>
+<td> 172 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 173 </td>
+<td> 2 </td>
+</tr>
+<tr>
+<td> 174 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 175 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 176 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 177 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 178 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 179 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 180 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 181 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 182 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 183 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 184 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 185 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 186 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 187 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 188 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 189 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 190 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 191 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 192 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 193 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 194 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 195 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 196 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 197 </td>
+<td> 8 </td>
+</tr>
+<tr>
+<td> 198 </td>
+<td> 6 </td>
+</tr>
+<tr>
+<td> 199 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 200 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 201 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 202 </td>
+<td> 18 </td>
+</tr>
+<tr>
+<td> 203 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 204 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 205 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 206 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 207 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 208 </td>
+<td> 7 </td>
+</tr>
+<tr>
+<td> 209 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 210 </td>
+<td> 9 </td>
+</tr>
+<tr>
+<td> 211 </td>
+<td> 18 </td>
+</tr>
+<tr>
+<td> 212 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 213 </td>
+<td> 18 </td>
+</tr>
+<tr>
+<td> 214 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 215 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 216 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 217 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 218 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 219 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 220 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 221 </td>
+<td> 20 </td>
+</tr>
+<tr>
+<td> 222 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 223 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 224 </td>
+<td> 22 </td>
+</tr>
+<tr>
+<td> 225 </td>
+<td> 16 </td>
+</tr>
+<tr>
+<td> 226 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 227 </td>
+<td> 19 </td>
+</tr>
+<tr>
+<td> 228 </td>
+<td> 23 </td>
+</tr>
+<tr>
+<td> 229 </td>
+<td> 26 </td>
+</tr>
+<tr>
+<td> 230 </td>
+<td> 25 </td>
+</tr>
+<tr>
+<td> 231 </td>
+<td> 24 </td>
+</tr>
+<tr>
+<td> 232 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 233 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 234 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 235 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 236 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 237 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 238 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 239 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 240 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 241 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 242 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 243 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 244 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 245 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 246 </td>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 247 </td>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 248 </td>
+<td> 19 </td>
+</tr>
+<tr>
+<td> 249 </td>
+<td> 6 </td>
+</tr>
+<tr>
+<td> 250 </td>
+<td> 21 </td>
+</tr>
+<tr>
+<td> 251 </td>
+<td> 25 </td>
+</tr>
+<tr>
+<td> 252 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 253 </td>
+<td> 24 </td>
+</tr>
+<tr>
+<td> 254 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 255 </td>
+<td> 23 </td>
+</tr>
+<tr>
+<td> 256 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 257 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 258 </td>
+<td> 18 </td>
+</tr>
+<tr>
+<td> 259 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 260 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 261 </td>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 262 </td>
+<td> 2 </td>
+</tr>
+<tr>
+<td> 263 </td>
+<td> 2 </td>
+</tr>
+<tr>
+<td> 264 </td>
+<td> 2 </td>
+</tr>
+<tr>
+<td> 265 </td>
+<td> 2 </td>
+</tr>
+<tr>
+<td> 266 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 267 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 268 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 269 </td>
+<td> 10 </td>
+</tr>
+<tr>
+<td> 270 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 271 </td>
+<td> 14 </td>
+</tr>
+<tr>
+<td> 272 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 273 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 274 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 275 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 276 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 277 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 278 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 279 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 280 </td>
+<td> 2 </td>
+</tr>
+<tr>
+<td> 281 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 282 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 283 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 284 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 285 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 286 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 287 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 288 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 289 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 290 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 291 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 292 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 293 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 294 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 295 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 296 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 297 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 298 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 299 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 300 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 301 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 302 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 303 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 304 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 305 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 306 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 307 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 308 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 309 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 310 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 311 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 312 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 313 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 314 </td>
+<td> 2 </td>
+</tr>
+<tr>
+<td> 315 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 316 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 317 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 318 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 319 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 320 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 321 </td>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 322 </td>
+<td> 11 </td>
+</tr>
+<tr>
+<td> 323 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 324 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 325 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 326 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 327 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 328 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 329 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 330 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 331 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 332 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 333 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 334 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 335 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 336 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 337 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 338 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 339 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 340 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 341 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 342 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 343 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 344 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 345 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 346 </td>
+<td> 1 </td>
+</tr>
+<tr>
+<td> 347 </td>
+<td> 1 </td>
+</tr>
+</tbody></table>
+<table><tbody>
+<tr>
+<th> Total Albums </th>
+</tr>
+<tr>
+<td> 346 </td>
+</tr>
+</tbody></table>
+Shortest song in the database
+<table><tbody>
+<tr>
+<th> Name </th>
+<th> Duration(seconds) </th>
+</tr>
+<tr>
+<td> É Uma Partida De Futebol </td>
+<td> 1 </td>
+</tr>
+</tbody></table>
+<table><tbody>
+<tr>
+<th> Average Duration(mins) </th>
+<th> Total Duration(mins) </th>
+</tr>
+<tr>
+<td> 6.5646350630733945 </td>
+<td> 22897 </td>
+</tr>
+</tbody></table>
+<table><tbody>
+<tr>
+<th> Total Genres </th>
+</tr>
+<tr>
+<td> 25 </td>
+</tr>
+</tbody></table>
+<table><tbody>
+<tr>
+<th> Total Songs </th>
+</tr>
+<tr>
+<td> 1293 </td>
+</tr>
+<tr>
+<td> 130 </td>
+</tr>
+<tr>
+<td> 371 </td>
+</tr>
+<tr>
+<td> 327 </td>
+</tr>
+<tr>
+<td> 12 </td>
+</tr>
+<tr>
+<td> 81 </td>
+</tr>
+<tr>
+<td> 579 </td>
+</tr>
+<tr>
+<td> 58 </td>
+</tr>
+<tr>
+<td> 48 </td>
+</tr>
+<tr>
+<td> 43 </td>
+</tr>
+<tr>
+<td> 15 </td>
+</tr>
+<tr>
+<td> 24 </td>
+</tr>
+<tr>
+<td> 28 </td>
+</tr>
+<tr>
+<td> 60 </td>
+</tr>
+<tr>
+<td> 30 </td>
+</tr>
+<tr>
+<td> 28 </td>
+</tr>
+<tr>
+<td> 34 </td>
+</tr>
+<tr>
+<td> 13 </td>
+</tr>
+<tr>
+<td> 92 </td>
+</tr>
+<tr>
+<td> 26 </td>
+</tr>
+<tr>
+<td> 64 </td>
+</tr>
+<tr>
+<td> 17 </td>
+</tr>
+<tr>
+<td> 40 </td>
+</tr>
+<tr>
+<td> 74 </td>
+</tr>
+<tr>
+<td> 1 </td>
+</tr>
+</tbody></table>
